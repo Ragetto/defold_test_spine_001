e1b3e0f3b3b38e3451f733b41851225792952c83
JC <jc.cambournac@gmail.com>
2022-05-06 00:26:49 +0200

    fully customizable characters (head/wrist/weapon) :)

--- gamedata/constants.lua
+++ gamedata/constants.lua
@@ -1,36 +1,9 @@
 local M = {}
 -------------------------------------------------------
 
-
-----------============---------
-----====== FUNCTIONAL =====-----
-----------============---------
-
-M.G_DEBUG = 1 -- JC 24/01/2021 00:08 # 0/1 => debug text displayed or not
-
-M.COLLECTION_CHAR_SELECTOR_NAME = "1.char_selector"
-M.COLLECTION_GAMEPLAY_NAME = "2.gameplay"
-
-M.CHARACTERS_QUANTITY = 3
-
-----------============---------
-----====== GAMEPLAY =====-----
-----------============---------
-
--- UX
-M.SCROLLING_MAX_NB_FRAMES = 3 -- JC 29/09/2021 21:02 # Used in the map scrolling (nb of frames recorded / inserted into a table)
-M.SCROLLING_SENSIVITY = 0.75  -- from 0.5 to 1
-M.SCROLLING_X = 1 -- 0: no / 1: yes
-M.SCROLLING_Y = 1 -- 0: no / 1: yes
-
--- Camera
-M.CAMERA_001 = "/camera_001"
-
-----------============---------
------====== MESSAGES =====-----
-----------============---------
-
-M.MSG_PLAYER_TO_BALL_PROPERTIES = hash("collided_player_param_to_ball")
-M.MSG_PLAYER_TO_BALL_PUNCH = hash("perfect punch")
+-- content (attachements)
+M.nb_heads = 3
+M.nb_weapons = 3
+M.nb_wrists = 2
 
 return M
---
+++ gamedata/runtime_gameplay.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.head_id_cur = 1
+M.wrist_id_cur = 1
+M.weapon_id_cur = 1
+
+M.item_id_cur = 0
+M.item_type_cur = ""
+M.item_slot_type_cur = ""
+
+M.switch_item = 0
+
+return M
